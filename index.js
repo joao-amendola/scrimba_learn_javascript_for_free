8c55be4ae8d2ee2a3015d0adf91668c41e5137cd
joao_amendola <joao_amendola@estudante.sesisenai.org.br>
2022-10-13 16:55:58 -0300

    Save button added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let countEl = document.getElementById("count-el")
-console.log(countEl)
 let count = 0
+
 function increment() {
     count += 1
     countEl.innerText = count
     console.log(`The button was clicked ${count} times`)
+}
+
+function save() {
+    console.log(count)
 }
